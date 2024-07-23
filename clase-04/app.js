e053684f45836ec963e3a381cd2755f3cb79c289
lfactorh <lfactorh99@gmail.com>
2024-07-23 15:55:51 -0500

    finished class 04

---
+++ clase-04/app.js
@@ -0,0 +1,29 @@
+import express, { json } from 'express';
+import { corsMiddleware } from './middlewares/cors.js';
+import { movieRouter } from './routes/movies.js';
+
+// EN EL FUTURO: El import del json será asi:
+// import movies from './movies.js' with { type: 'json' };
+
+/*
+  como leer un json en ESModule
+  import fs from 'node:fs';
+  const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'));
+*/
+
+// como leer un json recomendado por ahora
+
+// const movies = readJSON('./movies.json');
+
+const app = express();
+app.use(json());
+app.use(corsMiddleware());
+app.disable('x-powered-by');
+
+const PORT = process.env.PORT ?? 3000;
+
+app.use('/movies', movieRouter);
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en: http://localhost:${PORT}`);
+});
